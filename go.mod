a21d7b98666ad0d6880a9badeebef7acc40a7cd5
apanasyuk.kirill <apanasyuk.kirill@wb.ru>
2025-02-27 15:05:52 +0300

    another commit

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module movie.com
+module github.com/kirillApanasiuk/movie-movie
 
 go 1.23.1
--- pkg/model/model.go
+++ pkg/model/model.go
@@ -1,1 +1,6 @@
 package model
+
+type MovieDetails struct {
+	Rating   *float64 `json:"rating,omitempty"`
+	Metadata model.Metadata
+}
